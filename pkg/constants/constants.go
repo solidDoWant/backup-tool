dac3061a8fdb8435db9fc9eabe93f1a6bc312bde
solidDoWant <fred.heinecke@yahoo.com>
2025-01-29 07:51:13 +0000

    fix repo name
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 const (
 	ToolName      = "backup-tool"
 	Version       = "0.0.0-dev"
-	ImageRegistry = "ghcr.io/solidDoWant"
+	ImageRegistry = "ghcr.io/soliddowant"
 	ImageName     = ImageRegistry + "/" + ToolName
 	ImageTag      = Version
 	FullImageName = ImageName + ":" + ImageTag
